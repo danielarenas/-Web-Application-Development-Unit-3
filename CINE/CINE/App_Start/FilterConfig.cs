94c908c8b9ef4363fba2091143bff4db2d31ba20
danielarenas <54421882+danielarenas@users.noreply.github.com>
2020-11-10 18:39:31 -0800

    Practica de cine

---
+++ CINE/CINE/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CINE
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CINE/CINE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CINE.MvcApplication" Language="C#" %>
